52cfb66641f6935d178600d7697d3bab70f03f7a
caacastellanosc <caacastellanosc@correo.udistrital.edu.co>
2020-04-24 11:26:51 -0500

    filtrado de busqueda keyup+foreach+promises

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -11,21 +11,64 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-e9102.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[])  => {
-      this.productos = (resp);
-      this.cargando = false;
+
+    return new Promise( (resolve, reject) => {
+
+      this.http.get('https://angular-html-e9102.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[])  => {
+        this.productos = (resp);
+        this.cargando = false;
+        resolve();
+      });
+
     });
+
+
   }
 
   getProducto( id: string){
     return this.http.get(`https://angular-html-e9102.firebaseio.com/productos/${ id }.json`);
   }
 
+  buscarProducto( termino: string){
+
+    if (this.productos.length ===0){
+//cargar productos
+      this.cargarProductos().then(() =>{
+        //ejecutar despues de cargar productos
+        //aplicar filtro
+        this.filtrarProductos( termino);
+
+      });
+
+    }
+    else{
+      this.filtrarProductos( termino);
+    }
+
+
+  }
+
+  private filtrarProductos( termino: string){
+
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod =>{
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0  ){
+        this.productosFiltrado.push(prod);
+      }
+
+    });
+  }
 }
